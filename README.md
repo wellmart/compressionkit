2bbca2114b2823f6943568a90ef1246c99abde17
Wellington Marthas <well@dyntime.com>
2020-11-27 03:31:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# CompressionKit
+
+[![Build Status](https://travis-ci.org/wellmart/compressionkit.svg?branch=master)](https://travis-ci.org/wellmart/compressionkit)
+[![Swift 5](https://img.shields.io/badge/swift-5-blue.svg)](https://developer.apple.com/swift/)
+![Version](https://img.shields.io/badge/version-0.1.0-blue)
+[![Software License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+[![Swift Package Manager compatible](https://img.shields.io/badge/swift%20package%20manager-compatible-blue.svg)](https://github.com/apple/swift-package-manager)
+
+A lightweight kit to helps in data compression.
+
+## Requirements
+
+Swift 5 and beyond.
+
+## Usage
+
+```swift
+// coming soon
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Sources/Extensions/Data.swift
@@ -0,0 +1,125 @@
+//
+//  CompressionKit
+//
+//  Copyright (c) 2020 Wellington Marthas
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+import Compression
+import Adrenaline
+
+public extension Data {
+    @inlinable
+    func compressed() -> Data? {
+        guard !isEmpty else {
+            return self
+        }
+        
+        if #available(iOS 13, *) {
+            do {
+                return try (self as NSData).compressed(using: .lzfse) as Data
+            }
+            catch {
+                FirstChanceError.notify(error)
+                return nil
+            }
+        }
+        
+        return compressed(bufferSize: count)
+    }
+    
+    @inlinable
+    func decompressed() -> Data? {
+        guard !isEmpty else {
+            return self
+        }
+        
+        if #available(iOS 13, *) {
+            do {
+                return try (self as NSData).decompressed(using: .lzfse) as Data
+            }
+            catch {
+                FirstChanceError.notify(error)
+                return nil
+            }
+        }
+        
+        let bufferSize = count * 8
+        
+        for i in 1...4 {
+            if let data = decompressed(bufferSize: bufferSize * i) {
+                return data
+            }
+        }
+        
+        return nil
+    }
+}
+
+extension Data {
+    @usableFromInline
+    func compressed(bufferSize: Int) -> Data? {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        var length = 0
+        
+        defer {
+            buffer.deallocate()
+        }
+        
+        withUnsafeBytes { bytes in
+            guard let unsafePointer = bytes.bindMemory(to: UInt8.self).baseAddress else {
+                return
+            }
+            
+            length = compression_encode_buffer(buffer, bufferSize, unsafePointer, count, nil, COMPRESSION_LZFSE)
+        }
+        
+        guard length > 0 else {
+            return nil
+        }
+        
+        return Data(bytes: buffer, count: length)
+    }
+    
+    @usableFromInline
+    func decompressed(bufferSize: Int) -> Data? {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        var length = 0
+        
+        defer {
+            buffer.deallocate()
+        }
+        
+        withUnsafeBytes { bytes in
+            guard let unsafePointer = bytes.bindMemory(to: UInt8.self).baseAddress else {
+                return
+            }
+            
+            length = compression_decode_buffer(buffer, bufferSize, unsafePointer, count, nil, COMPRESSION_LZFSE)
+        }
+        
+        guard length > 0 else {
+            return nil
+        }
+        
+        return Data(bytes: buffer, count: length)
+    }
+}
